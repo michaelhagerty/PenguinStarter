d9f8a4d0ddfffc0d8bbd4ce98dbbdc1ccf0a2f7e
Michael Hagerty <michael.hagerty@Michaels-MacBook-Air.local>
2020-04-05 16:35:27 -0400

    Lab 11

--- penguinstarter.js
+++ penguinstarter.js
@@ -1,8 +1,44 @@
 var penguinPromise = d3.json("classData.json")
 penguinPromise.then(
 function(penguins)
-{console.log("worked",penguins)
+{console.log("worked",penguins);
+ drawTable(penguins);
 },
 function(err)
     {console.log("failed",err);}
 )
+
+
+var getGrade = function(quiz)
+    {
+        return quizes.grade
+    }
+var givenmean = function(penguin)
+    {
+        var allquizgrades = penguin.quizes.map(getGrade)
+        var findquizmean = d3.mean(allquizgrades)
+        return findquizmean
+    }
+var allquizmean = function(classroom)
+    {
+    var arrayquizmeans = classroom.map(givenmean)
+    return arrayquizmeans
+}
+console.log(allquizmean)
+
+
+
+var drawTable = function(penguins)
+{
+    var rows = d3.select("penguinTable")
+    .selectAll("tr")
+        .data(penguins)
+        .enter()
+        .append("tr")
+    
+    rows.append("td")
+        .append("img")
+        .attr("src", function(penguins){return penguins.picture})
+    rows.append("td")
+        
+}
